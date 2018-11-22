a8209d497d712a25cdd5f7225e96f1e508204d24
hcli <ilargehadroncollider@gmail.com>
2018-11-22 13:55:25 -0600

    print confusion matrix for part1

---
+++ print_results.py
@@ -0,0 +1,27 @@
+import numpy as np
+from helperFunctions import getUCF101
+NUM_CLASSES = 101
+
+
+def print_confusion_matrix(class_list, file_path='single_frame_confusion_matrix.npy'):
+    confusion_matrix = np.load(file_path)
+    number_of_examples = np.sum(confusion_matrix, axis=1)
+    for i in range(NUM_CLASSES):
+        confusion_matrix[i, :] = confusion_matrix[i, :] / np.sum(confusion_matrix[i, :])
+
+    results = np.diag(confusion_matrix)
+    indices = np.argsort(results)
+
+    sorted_list = np.asarray(class_list)
+    sorted_list = sorted_list[indices]
+    sorted_results = results[indices]
+
+    for i in range(NUM_CLASSES):
+        # print(sorted_list[i], sorted_results[i], number_of_examples[indices[i]])
+        print("%s & %.4f & %d" %(sorted_list[i], sorted_results[i], int(number_of_examples[indices[i]])))
+
+
+def main():
+    data_directory = '/projects/training/bauh/AR/'
+    class_list, train, test = getUCF101(base_directory=data_directory)
+    print_confusion_matrix(class_list=class_list)
